c16d0adea0b92da3ea0e66396c42799903413e25
Mikkel Gravgaard <mikkel@klokke.dk>
2014-12-19 16:21:29 +0100

    check child view controllers and all navigation controller's view controllers

--- Pod/Classes/BTFLeakDetect.m
+++ Pod/Classes/BTFLeakDetect.m
@@ -7,10 +7,16 @@
 #import <UIKit/UIKit.h>
 #import "BTFLeakDetect.h"
 
+@interface BTFLeakDetect (Private)
++ (void)checkViewController:(__weak UIViewController *)vc;
+@end
+
 static BOOL ThrowException = NO;
 
+void checkIfNil(__weak id obj, NSInteger seconds);
+
 @implementation NSObject (Swizzling)
-+ (void)swizzle:(SEL)originalSelector with:(SEL)newSelector
++ (void)btf_swizzle:(SEL)originalSelector with:(SEL)newSelector
 {
     Class aClass = [self class];
     Method origMethod = class_getInstanceMethod(aClass, originalSelector);
@@ -29,6 +35,25 @@ static BOOL ThrowException = NO;
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "InfiniteRecursion"
 
+
+- (void)btf_dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion {
+
+    [BTFLeakDetect checkViewController:self.presentedViewController];
+
+    [self btf_dismissViewControllerAnimated:flag completion:completion];
+}
+@end
+
+@implementation UINavigationController (Swizzled)
+- (UIViewController *)btf_popViewControllerAnimated:(BOOL)animated {
+
+    [BTFLeakDetect checkViewController:[self.viewControllers lastObject]];
+
+    return [self btf_popViewControllerAnimated:animated];
+}
+@end
+#pragma clang diagnostic pop
+
 void checkIfNil(__weak id obj, NSInteger seconds){
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, seconds * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         if(ThrowException){
@@ -41,25 +66,23 @@ void checkIfNil(__weak id obj, NSInteger seconds){
 
 }
 
-- (void)sw_dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion {
-
-    __weak UIViewController *vc = self.presentedViewController;
-    checkIfNil(vc, 2);
-    [self sw_dismissViewControllerAnimated:flag completion:completion];
-}
-@end
-
-@implementation UINavigationController (Swizzled)
-- (UIViewController *)sw_popViewControllerAnimated:(BOOL)animated {
-    __weak UIViewController *vc = [self.viewControllers lastObject];
-    checkIfNil(vc, 2);
-    return [self sw_popViewControllerAnimated:animated];
-}
-@end
-#pragma clang diagnostic pop
-
-
 @implementation BTFLeakDetect {
+
+}
+
++ (void)checkViewController:(__weak UIViewController *)vc
+{
+    NSMutableSet *vcs = [NSMutableSet setWithObject:vc];
+
+    if([vc isKindOfClass:[UINavigationController class]]){
+        UINavigationController *nvc = (UINavigationController *) vc;
+        [vcs addObjectsFromArray:nvc.viewControllers];
+    }
+    [vcs addObjectsFromArray:vc.childViewControllers];
+
+    for(UIViewController *cvc in vcs){
+        checkIfNil(cvc, 2);
+    }
 
 }
 
@@ -75,8 +98,8 @@ void checkIfNil(__weak id obj, NSInteger seconds){
 }
 
 + (void)enable{
-    [UINavigationController swizzle:@selector(popViewControllerAnimated:) with:@selector(sw_popViewControllerAnimated:)];
-    [UIViewController swizzle:@selector(dismissViewControllerAnimated:completion:) with:@selector(sw_dismissViewControllerAnimated:completion:)];
+    [UINavigationController btf_swizzle:@selector(popViewControllerAnimated:) with:@selector(btf_popViewControllerAnimated:)];
+    [UIViewController btf_swizzle:@selector(dismissViewControllerAnimated:completion:) with:@selector(btf_dismissViewControllerAnimated:completion:)];
 
 }
 
